1ad14e532509fc34038f81e6e1ff747c9d9742c0
Abahimoni <iduh12000@yahoo.com>
2023-10-06 09:35:56 -0700

    factory function
    function and the return keyword

---
+++ script.js
@@ -0,0 +1,71 @@
+
+
+function Namefunc() {//declaration or function statement
+    console.dir('learning function');
+}
+
+
+let Namefuny = function scho() {//Named function expression
+    console.log('lecturing my students');
+}
+
+let animal = {
+
+    kingdom: 'Bauchi',
+    owner: 'Nigeria',
+    tribe: 'Basayi',
+};
+
+console.log(animal.owner);
+
+
+animal.forest ='yankari';
+console.log(animal);
+let country = new Object();
+country.state = 'Benue';
+console.log(country);
+
+
+let home = {
+    address: 'lugbe',
+    contact: 'sam',
+    owner: "shroeder",
+    date: 2005,
+    student: 'victor',
+}
+
+var student = 'Taiwo';
+
+function learingwell() {
+    return this.student + ' is learning javacript and php';
+}
+
+//console.log(learingwell.call(home));
+
+
+
+
+
+
+
+let estate = {//Dot Notation
+        name: 'flat',
+        student: 'Mayowa',
+        numberofHouse: '45P',
+        address: "danta left 454",
+        owner: "Abah Moses Imoni Iduh",
+        date: 2001,
+         samson() {
+            return this.owner+" built this house in the year "+ this.date;
+        }
+
+    };
+
+  let 
+
+
+
+
+
+
+
